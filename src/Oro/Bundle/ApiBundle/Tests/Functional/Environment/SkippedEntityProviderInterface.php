a4bab609f1e8698fb70cf24691071d4812db6794
Jenkins <jenkins@example.com>
2023-05-11 01:32:29 +0200

    Code cleanup

--- src/Oro/Bundle/ApiBundle/Tests/Functional/Environment/SkippedEntityProviderInterface.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-namespace Oro\Bundle\ApiBundle\Tests\Functional\Environment;
-
-interface SkippedEntityProviderInterface
-{
-    public function isSkippedEntity(string $entityClass, string $action): bool;
-}
